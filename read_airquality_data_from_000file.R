7cc4c198679effde7f65b5d00b2e658e2a82eee8
ny0118 <ny0118_happy@126.com>
2020-03-25 15:37:12 +0800

    判断是否闰年；自动标记每天文件，以便计算日均值

--- read_airquality_data_from_000file.R
+++ read_airquality_data_from_000file.R
@@ -2,13 +2,15 @@
 # Author: Yang Nan
 # 2020-03-24
 
+dir='F:/AQI/2014/'
+
 #(1)read 000 file and get data------------------------
 #set the working directory
 getwd()
 setwd("F:/AQI/2014/")
 
 #find all files in this directory
-dirname<-list.files('F:/AQI/2014/',full.names=T,all.files=T,recursive=T)
+dirname<-list.files(dir,full.names=T,all.files=T,recursive=T)
 dirname[1]
 length(dirname)
 
@@ -34,8 +36,15 @@ for (n in 1:length(dirname)) {
 year<-floor(YYYYMMDDHH[1]/1000000)
 MMDDHH<-YYYYMMDDHH-year*1e6
 
+day_in_month<-c(31,28,31,30,31,30,31,31,30,31,30,31)#非闰年
+day_in_month_leap<-c(31,29,31,30,31,30,31,31,30,31,30,31)#非闰年
 #chek if year is leap year (2016 is leap year in 2014-2019)
-if (year== 2016) days=366 else  days=365
+if (year== 2016){
+  days=366 
+  day_in_month<-day_in_month_leap
+} else{
+  days=365
+}
 
 #derive day_file_num_flag
 dayID<-c(1:days)
@@ -46,20 +55,25 @@ dayID_end[]<-NA
 
 day_file_num_flag<-data.frame(dayID,dayID_start,dayID_end)
 
-#Jan 31 days
-for (n in 1:31) {
-  day_num<-file_num[MMDDHH>=(10000+100*n) &MMDDHH<(10100+100*n)]
-  if (day_num >=1){
-    day_file_num_flag$dayID_start[n]<-day_num[1]
-    day_file_num_flag$dayID_end[n]<-day_num[length(day_num)]   
-  }
-}
-#Feb 28 days
-for (n in 1:28) {
-  day_num<-file_num[MMDDHH>=(20000+100*n) &MMDDHH<(20100+100*n)]
-  if (day_num >=1){
-    day_file_num_flag$dayID_start[31+n]<-day_num[1]
-    day_file_num_flag$dayID_end[31+n]<-day_num[length(day_num)]   
+
+#derive all day_file_num_flag in a year
+for (m in 1:12) {
+  for (n in 1:day_in_month[m]) {
+    floor_file<-m*10000+100*n
+    ceiling_file<-m*10000+100*(n+1)
+    day_num<-file_num[MMDDHH>=(floor_file) &MMDDHH<(ceiling_file)]
+    if (day_num >=1){
+      flag=n
+      if (m==1) flag=n
+      if (m > 1){
+        for (k in 1:(m-1)) {
+          flag=flag+day_in_month[k]
+        }
+      }
+      day_file_num_flag$dayID_start[flag]<-day_num[1]
+      day_file_num_flag$dayID_end[flag]<-day_num[length(day_num)]   
+    }
   }
 }
 
+print(year)
